8285f7866ac0370346e968797adb076d7d850dae
drenski666 <drenski666@hotmail.com>
2024-05-09 08:39:21 +0300

    Guid ProductId

--- src/ProductCatalog.API/Program.cs
+++ src/ProductCatalog.API/Program.cs
@@ -12,13 +12,13 @@ app.MapGet("/products", () =>
 {
     return new List<Product>
     {
-        new Product("MacBook Pro", 2999.99M),
-        new Product("Harry Potter and The Order of The Phoenix", 19.99M),
-        new Product("Leather Jacket", 699.99M),
+        new Product(Guid.NewGuid().ToString(), "MacBook Pro", 2999.99M),
+        new Product(Guid.NewGuid().ToString(), "Harry Potter and The Order of The Phoenix", 19.99M),
+        new Product(Guid.NewGuid().ToString(), "Leather Jacket", 699.99M),
     };
 });
 
 app.Run();
 
-internal record Product(string Name, decimal Price);
+internal record Product(string ProductId, string Name, decimal Price);
 
