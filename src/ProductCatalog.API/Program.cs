ed82d4c2a271f23ae70615f9c225a0564b3827a0
drenski666 <drenski666@hotmail.com>
2024-05-09 08:44:19 +0300

    Get product by id

--- src/ProductCatalog.API/Program.cs
+++ src/ProductCatalog.API/Program.cs
@@ -8,17 +8,24 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+List<Product> products = new List<Product>
+    {
+        new Product(Guid.NewGuid().ToString(), "MacBook Pro", 2999.99M, 5),
+        new Product(Guid.NewGuid().ToString(), "Harry Potter and The Order of The Phoenix", 19.99M, 3),
+        new Product(Guid.NewGuid().ToString(), "Leather Jacket", 699.99M, 1),
+    };
+
 app.MapGet("/products", () =>
 {
-    return new List<Product>
-    {
-        new Product(Guid.NewGuid().ToString(), "MacBook Pro", 2999.99M),
-        new Product(Guid.NewGuid().ToString(), "Harry Potter and The Order of The Phoenix", 19.99M),
-        new Product(Guid.NewGuid().ToString(), "Leather Jacket", 699.99M),
-    };
+    return products;
+});
+
+app.MapGet("/products/{productId}", (string productId) =>
+{
+    return products.SingleOrDefault(products => products.ProductId == productId);
 });
 
 app.Run();
 
-internal record Product(string ProductId, string Name, decimal Price);
+internal record Product(string ProductId, string Name, decimal Price, int Quantitys);
 
